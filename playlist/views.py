184c8e1fc6a4bdae5e4647fe8946eb5c82e172ea
Eric Ly <eric.ly906@berkeley.edu>
2022-03-23 14:12:24 -0700

    access token

--- playlist/views.py
+++ playlist/views.py
@@ -38,7 +38,9 @@ def home(request):
     data['grant_type'] = "client_credentials"
 
     r = requests.post(url, headers=headers, data=data)
-
+    print(r)
+    if r.status_code != 200:
+        return False
     token = r.json()['access_token']
 
     artist_data = Artist.objects.all()
